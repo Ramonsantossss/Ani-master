663c4a8497bd42aafb559e9c27a68e6b75b56a78
Apenas-de <103340294+Ramonsantossss@users.noreply.github.com>
2023-05-16 11:27:53 -0300

    Update URLS.js

--- utils/URLS.js
+++ utils/URLS.js
@@ -1,8 +1,8 @@
 export const URL = {
-  POPULAR: "https://minha-apiakkk.onrender.com/api/" + "popular/",
-  RECENT: "https://minha-apiakkk.onrender.com/api/" + "recentlyadded/",
-  DETAILS: "https://minha-apiakkk.onrender.com/api/" + "details/",
-  EPLINK: "https://minha-apiakkk.onrender.com/api/" + "watching/",
-  GENRES: "https://minha-apiakkk.onrender.com/api/" + "genre/",
-  SEARCH: "https://minha-apiakkk.onrender.com/api/" + "search/",
+  POPULAR: "https://minha-apiakkk.onrender.com/api/popular/",
+  RECENT: "https://minha-apiakkk.onrender.com/api/recentlyadded/",
+  DETAILS: "https://minha-apiakkk.onrender.com/api/details/",
+  EPLINK: "https://minha-apiakkk.onrender.com/api/watching/",
+  GENRES: "https://minha-apiakkk.onrender.com/api/genre/",
+  SEARCH: "https://minha-apiakkk.onrender.com/api/search/"
 };
